0349c2a7c368259473e9389fdb7299b686ebd745
alafsasa <alafsasa@gmail.com>
2023-02-08 17:38:16 +0300

    upload image form field

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import './App.css';
 
+type InputEvent = React.ChangeEvent<HTMLInputElement>
+
 function App() {
+  const handleFileChanges = (e: InputEvent) => {
+  }
   return (
-    <div className="App">
+    <div className="container">
+      <div className='row'>
+        <div className='col-sm-12'>
+          <div className='display-2'>Lue-image-Editor</div>
+          <canvas height={600} width={400} style={{backgroundColor: '#ddd', border: '2px solid blue'}}></canvas>
+          <div>
+            <input type="file" accept='image/*' onChange={handleFileChanges} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
